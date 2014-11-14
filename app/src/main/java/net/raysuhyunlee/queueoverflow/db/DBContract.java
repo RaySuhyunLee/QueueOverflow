9b77564b08cc44b8b5025e1a7ad4f2f59073a9b5
Suhyun Lee <raysuhyunlee@gmail.com>
2014-11-14 11:50:38 +0900

    - put DBHelper class into DBContract class and changed it's name to DBOpenHelper. - implemented insert, delete, read functions for table Task, in DBContract class.

--- app/src/main/java/net/raysuhyunlee/queueoverflow/db/DBContract.java
+++ app/src/main/java/net/raysuhyunlee/queueoverflow/db/DBContract.java
@@ -1,18 +1,28 @@
 package net.raysuhyunlee.queueoverflow.db;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 
 /**
  * Created by SuhyunLee on 14. 11. 11..
  */
 public class DBContract {
+
+    public static final String SQL_CREATE_TASK =
+            "CREATE_TABLE " + Task.TABLE_NAME + "(" +
+                    Task.COLUMN_NAME_TASK_NAME + " VARCHAR(30)," +
+                    Task.COLUMN_NAME_TASK_DATE + " DATETIME);";
+
     // To prevent someone from accidentally instantiating the contract class,
     // give it an empty constructor.
     public DBContract() {}
 
     public static abstract class Task implements BaseColumns {
         public static final String TABLE_NAME = "table_task";
-        public static final String COLUMN_NAME_TASK = "name";
-        public static final String COLUMN_NAME_DATE = "date";
+        public static final String COLUMN_NAME_TASK_NAME = "task_name";
+        public static final String COLUMN_NAME_TASK_DATE = "task_date";
+
     }
 }
