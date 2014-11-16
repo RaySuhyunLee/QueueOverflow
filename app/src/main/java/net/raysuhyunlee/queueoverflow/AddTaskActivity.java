eed947ce5761879e809d6f16720d2585ebe4cfec
Suhyun Lee <raysuhyunlee@gmail.com>
2014-11-16 14:05:50 +0900

    - added dialogs for setting date and time

--- app/src/main/java/net/raysuhyunlee/queueoverflow/AddTaskActivity.java
+++ app/src/main/java/net/raysuhyunlee/queueoverflow/AddTaskActivity.java
@@ -1,20 +1,54 @@
 package net.raysuhyunlee.queueoverflow;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+
+import net.raysuhyunlee.queueoverflow.db.DBAdapter;
+
+import java.util.Calendar;
 
 /**
  * Created by Suhyun Lee on 2014-11-14.
  */
 public class AddTaskActivity extends BaseActivity {
+    DeadlineManager deadlineManager;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        deadlineManager = new DeadlineManager();
+
+        setToolbar();
+
+        TextView textView_date = (TextView)findViewById(R.id.textView_date);
+        TextView textView_time = (TextView)findViewById(R.id.textView_time);
+        textView_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+        textView_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog();
+            }
+        });
+    }
+
+    public void setToolbar() {
         Toolbar toolbar = initializeToolbar();
-        toolbar.setNavigationIcon(R.drawable.icon_action_cancel);
+        toolbar.setNavigationIcon(R.drawable.abc_ic_clear_mtrl_alpha);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,9 +58,48 @@ public class AddTaskActivity extends BaseActivity {
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
+                switch(menuItem.getItemId()) {
+                    case R.id.action_save:
+                        /*DBAdapter dbAdapter = new DBAdapter(AddTaskActivity.this);
+                        EditText editText_taskName = (EditText)findViewById(R.id.editText_taskName);
+                        dbAdapter.insertTask(editText_taskName.getText().toString(),
+                                deadlineManager.getDeadline());*/
+                        finish();
+                }
                 return false;
             }
         });
         toolbar.inflateMenu(R.menu.activity_add_task);
+    }
+
+    public void showDatePickerDialog() {
+        new MaterialDialog.Builder(this)
+                .customView(R.layout.dialog_date_picker)
+                .build()
+                .show();
+    }
+
+    public void showTimePickerDialog() {
+        new MaterialDialog.Builder(this)
+                .customView(R.layout.dialog_time_picker)
+                .build()
+                .show();
+    }
+
+    private class DeadlineManager {
+        private Calendar deadline;
+        public DeadlineManager() {
+            deadline = Calendar.getInstance();
+        }
+        public void setDeadlineDate(int year, int month, int dayOfMonth) {
+            deadline.set(year, month, dayOfMonth);
+        }
+        public void setDeadlineTime(int hourOfDay, int minute) {
+            deadline.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            deadline.set(Calendar.MINUTE, minute);
+        }
+        public Calendar getDeadline() {
+            return (Calendar)deadline.clone();
+        }
     }
 }
