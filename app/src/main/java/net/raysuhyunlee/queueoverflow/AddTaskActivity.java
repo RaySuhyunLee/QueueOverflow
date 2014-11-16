0a084e8e8762cbee1834fe00d70961ccccc04d8e
Suhyun Lee <raysuhyunlee@gmail.com>
2014-11-16 23:57:22 +0900

    - implemented transaction between AddTaskActivity and Date/Time Picker Dialog

--- app/src/main/java/net/raysuhyunlee/queueoverflow/AddTaskActivity.java
+++ app/src/main/java/net/raysuhyunlee/queueoverflow/AddTaskActivity.java
@@ -6,14 +6,17 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 
 import net.raysuhyunlee.queueoverflow.db.DBAdapter;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 /**
@@ -44,6 +47,8 @@ public class AddTaskActivity extends BaseActivity {
                 showTimePickerDialog();
             }
         });
+        resetTextViewDate();
+        resetTextViewTime();
     }
 
     public void setToolbar() {
@@ -73,17 +78,65 @@ public class AddTaskActivity extends BaseActivity {
     }
 
     public void showDatePickerDialog() {
+        View dialog_date_picker = View.inflate(this, R.layout.dialog_date_picker, null);
+        {
+            DatePicker datePicker = (DatePicker) dialog_date_picker.findViewById(R.id.datePicker);
+            Calendar deadline = deadlineManager.getDeadline();
+            datePicker.init(deadline.get(Calendar.YEAR),
+                    deadline.get(Calendar.MONTH),
+                    deadline.get(Calendar.DAY_OF_MONTH),
+                    null);
+        }
+
         new MaterialDialog.Builder(this)
-                .customView(R.layout.dialog_date_picker)
+                .customView(dialog_date_picker)
+                .callback(new MaterialDialog.SimpleCallback() {
+                    @Override
+                    public void onPositive(MaterialDialog dialog) {
+                        DatePicker datePicker = (DatePicker)dialog.getCustomView().findViewById(R.id.datePicker);
+                        deadlineManager.setDeadlineDate(datePicker.getYear(),
+                                datePicker.getMonth(),
+                                datePicker.getDayOfMonth());
+                        resetTextViewDate();
+                    }
+                })
                 .build()
                 .show();
     }
 
     public void showTimePickerDialog() {
+        View dialog_time_picker = View.inflate(this, R.layout.dialog_time_picker, null);
+        {
+            TimePicker timePicker = (TimePicker) dialog_time_picker.findViewById(R.id.timePicker);
+            Calendar deadline = deadlineManager.getDeadline();
+            timePicker.setCurrentHour(deadline.get(Calendar.HOUR_OF_DAY));
+            timePicker.setCurrentMinute(deadline.get(Calendar.MINUTE));
+        }
         new MaterialDialog.Builder(this)
-                .customView(R.layout.dialog_time_picker)
+                .customView(dialog_time_picker)
+                .callback(new MaterialDialog.SimpleCallback() {
+                    @Override
+                    public void onPositive(MaterialDialog dialog) {
+                        TimePicker timePicker = (TimePicker)dialog.getCustomView().findViewById(R.id.timePicker);
+                        deadlineManager.setDeadlineTime(timePicker.getCurrentHour(),
+                                timePicker.getCurrentMinute());
+                        resetTextViewTime();
+                    }
+                })
                 .build()
                 .show();
+    }
+
+    public void resetTextViewDate() {
+        TextView textView_date = (TextView)findViewById(R.id.textView_date);
+        SimpleDateFormat dateFormat = new SimpleDateFormat(getString(R.string.date_format));
+        textView_date.setText(dateFormat.format(deadlineManager.getDeadline().getTime()));
+    }
+
+    public void resetTextViewTime() {
+        TextView textView_time = (TextView)findViewById(R.id.textView_time);
+        SimpleDateFormat timeFormat = new SimpleDateFormat(getString(R.string.time_format));
+        textView_time.setText(timeFormat.format(deadlineManager.getDeadline().getTime()));
     }
 
     private class DeadlineManager {
